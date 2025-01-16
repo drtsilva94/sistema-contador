d7983314a85b000824b7141e371f083faa904f68
Anna <anna.drt94@gmail.com>
2025-01-16 17:10:51 -0300

    Commit inicial

---
+++ sistemacontador/src/ContadorApp.java
@@ -0,0 +1,32 @@
+package br.com.sistemacontador;
+
+import java.util.Scanner;
+
+public class ContadorApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Digite o primeiro número:");
+            int numero1 = scanner.nextInt();
+
+            System.out.println("Digite o segundo número:");
+            int numero2 = scanner.nextInt();
+
+            if (numero1 > numero2) {
+                throw new IllegalArgumentException("O primeiro número deve ser menor ou igual ao segundo.");
+            }
+
+            System.out.println("Contagem:");
+            for (int i = numero1; i <= numero2; i++) {
+                System.out.println(i);
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Entrada inválida. Por favor, insira números inteiros.");
+        } finally {
+            scanner.close();
+        }
+    }
+}
